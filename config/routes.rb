353c956bb39e06065444256f7818f0f6e39a1d65
Adlai <adlai.bradley@gmail.com>
2019-09-06 16:52:51 -0400

    made styling changes. changed root to todo_items

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'todo_items/index'
+
   resources :todo_items
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'todo_items#index'
 end
